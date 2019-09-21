8ea3baa709c82f4f8404e00522560e1ed5294a9e
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-21 16:11:09 +0900

    recordMap.block 이 필요한 것으로 보임
    - 모든 data 가 이 아래에 있고,
    - https://github.com/zeit/spr-landing 에서도 그러함

--- __tests__/notion.ts
+++ __tests__/notion.ts
@@ -1,15 +1,9 @@
 import { loadPageChunk } from "../data/notion";
-import fs from "fs";
 
 test("check api call", async () => {
   const POST_PAGE_ID = "6a400436-ae73-464e-acbc-070fdf8d990f";
 
   const loaded = await loadPageChunk({ pageId: POST_PAGE_ID });
 
-  fs.writeFileSync(
-    "./loadChunk_api_response.json",
-    JSON.stringify(loaded, null, 2)
-  );
-
-  expect(loaded).toBeTruthy();
+  expect(loaded.recordMap.block).toBeTruthy();
 });
