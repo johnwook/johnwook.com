8fb2c2f1cbec6456c7ec8829d5fac63a1ab91f1a
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-21 16:04:50 +0900

    response 를 이용하기 위해 우선 저장

--- __tests__/notion.ts
+++ __tests__/notion.ts
@@ -1,11 +1,15 @@
 import { loadPageChunk } from "../data/notion";
+import fs from "fs";
 
 test("check api call", async () => {
   const POST_PAGE_ID = "6a400436-ae73-464e-acbc-070fdf8d990f";
 
   const loaded = await loadPageChunk({ pageId: POST_PAGE_ID });
 
-  console.dir(loaded);
+  fs.writeFileSync(
+    "./loadChunk_api_response.json",
+    JSON.stringify(loaded, null, 2)
+  );
 
   expect(loaded).toBeTruthy();
 });
