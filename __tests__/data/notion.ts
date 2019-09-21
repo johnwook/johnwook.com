90e54520685a3b9789cc6ff597aaa4a0cb8d82ef
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-21 17:06:37 +0900

    dir 위치를 적절히 변경함
    - 이전 commit 에서 빼먹은 json response 추가

---
+++ __tests__/data/notion.ts
@@ -0,0 +1,11 @@
+import { loadPageChunk } from "../../data/notion";
+
+test("check api call", async () => {
+  const POST_PAGE_ID = "6a400436-ae73-464e-acbc-070fdf8d990f";
+
+  console.log(loadPageChunk);
+
+  const pageChunk = await loadPageChunk({ pageId: POST_PAGE_ID });
+
+  expect(pageChunk).toBeTruthy();
+});
