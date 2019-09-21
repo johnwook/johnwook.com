574a10cfaa2c4dcc6edfb0afd70e04069ac274c5
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-21 16:21:52 +0900

    block 을 object 로 변경시키는 작업이 필요해 보임

--- __tests__/notion.ts
+++ __tests__/notion.ts
@@ -3,7 +3,11 @@ import { loadPageChunk } from "../data/notion";
 test("check api call", async () => {
   const POST_PAGE_ID = "6a400436-ae73-464e-acbc-070fdf8d990f";
 
-  const loaded = await loadPageChunk({ pageId: POST_PAGE_ID });
+  const pageChunk = await loadPageChunk({ pageId: POST_PAGE_ID });
 
-  expect(loaded.recordMap.block).toBeTruthy();
+  const blocks = pageChunk.recordMap.block;
+
+  const keys = Object.keys(blocks);
+
+  expect(keys.length).toBeTruthy();
 });
