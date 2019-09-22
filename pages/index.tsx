9a4f81a7d03ffa8a9cba9e29fc416130e9a354b9
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-22 15:38:43 +0900

    remove non-related image

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,6 @@ const Home: NextPage<Props> = ({ body }) => (
         The goal, 치아 교정
       </a>
     </div>
-
-    <img src="/api/image?url=https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F4fab55f6-ea1c-48e7-b0de-46957d18c9d5%2Fdaniel2.jpg" />
   </div>
 );
 
