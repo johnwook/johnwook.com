0061659362616b355b4d8ff3029332904b9533c3
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-25 22:56:31 +0900

    copy _document from
    - https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js

---
+++ pages/_document.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from "next/document";
+import { ServerStyleSheets } from "@material-ui/styles";
+import theme from "../theme";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />)
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [
+        <React.Fragment key="styles">
+          {initialProps.styles}
+          {sheets.getStyleElement()}
+        </React.Fragment>
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+          <meta name="theme-color" content={theme.palette.primary.main} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
