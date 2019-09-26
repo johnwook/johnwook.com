c998721d38b308912563b874e621da04d3baa233
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-26 23:16:17 +0900

    App bar 적용

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,24 @@
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import Head from "next/head";
 
 const Layout = ({ children }) => (
-  <Container maxWidth="sm">
-    <Head>
-      <title>johnwook.com</title>
-    </Head>
-    {children}
-  </Container>
+  <Box>
+    <AppBar position="sticky" color="default">
+      <Toolbar>
+        <Typography variant="h6">Johnwook.com</Typography>
+      </Toolbar>
+    </AppBar>
+    <Container maxWidth="sm">
+      <Head>
+        <title>johnwook.com</title>
+      </Head>
+      {children}
+    </Container>
+  </Box>
 );
 
 export default Layout;
