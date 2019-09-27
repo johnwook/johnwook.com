59baf3e067b51728cdacf232aeff1a8f8cfa91fe
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-09-27 15:53:03 +0900

    layout 정리

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,16 @@ import Link from "next/link";
 import { NextPage } from "next";
 
 import fetch from "cross-fetch";
+import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Paper from "@material-ui/core/Paper";
 
 import Layout from "../components/layout";
 import { HomeData } from "../data/home";
@@ -17,22 +23,28 @@ type Props = HomeData;
 
 const Home: NextPage<Props> = ({ posts, sections }) => (
   <Layout>
-    <Card>
-      <CardMedia component="img" image={sections[0].value} />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {sections[1].value}
-        </Typography>
-      </CardContent>
-    </Card>
+    <Box mt={1}>
+      <Card>
+        <CardMedia component="img" image={sections[0].value} />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {sections[1].value}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
 
-    <div>
-      {posts.map(p => (
-        <Link key={p.id} href={"/posts/[pid]"} as={`/posts/${p.id}`}>
-          <a>{p.title}</a>
-        </Link>
-      ))}
-    </div>
+    <Box mt={1}>
+      <Paper>
+        <List aria-label="posts">
+          {posts.map(post => (
+            <ListItem button key={post.id}>
+              <ListItemText primary={post.title} />
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    </Box>
   </Layout>
 );
 
