a8533847d8b529d0a3b6cb24668965636363f314
Johnwook Choi <johnwook.choi@gmail.com>
2019-10-04 17:11:19 +0900

    title 에 grid layout 적용

--- components/postListItemTitle.tsx
+++ components/postListItemTitle.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 import PostListDate from "./postListDate";
@@ -11,12 +12,16 @@ interface Props {
 
 const PostListItemTitle = ({ createdTime, title }: Props) => {
   return (
-    <div>
-      <PostListDate timestamp={createdTime} />
-      <Typography component="span" variant="subtitle1">
-        {" " + title}
-      </Typography>
-    </div>
+    <Grid container wrap="nowrap">
+      <Grid item xs={10} zeroMinWidth>
+        <Typography variant="subtitle1" noWrap>
+          {" " + title}
+        </Typography>
+      </Grid>
+      <Grid item xs={2}>
+        <PostListDate timestamp={createdTime} />
+      </Grid>
+    </Grid>
   );
 };
 
