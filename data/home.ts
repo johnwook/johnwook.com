c89170fc734c67ad7e46e1ec9217f5f449d1501c
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-10-08 23:18:06 +0900

    home 에서 사용하는 data 를 좀 더 명확히 함

--- data/home.ts
+++ data/home.ts
@@ -2,13 +2,15 @@ import { loadPageChunk, queryCollection } from "./notion";
 import {
   convertBlock,
   convertCollection,
-  ConvertBlockOutput,
-  ConvertCollectionOutput
+  ConvertCollectionOutput,
+  TextBlock,
+  ImageBlock
 } from "./convert";
 
 interface Output {
+  cardImage: ImageBlock;
+  cardText: TextBlock;
   posts: ConvertCollectionOutput;
-  sections: ConvertBlockOutput[];
 }
 
 const pageId = "e740a8ab-2c00-4ea7-8fa0-54c678d40075";
@@ -23,6 +25,13 @@ export const getData = async (): Promise<Output> => {
   const contentIds: string[] = page.value.content;
 
   const homeBlocks = contentIds.map(id => blocks[id]);
+
+  const cardImage = convertBlock(
+    homeBlocks.find(b => b.value.type === "image")
+  ) as ImageBlock;
+  const cardText = convertBlock(
+    homeBlocks.find(b => b.value.type === "text")
+  ) as TextBlock;
 
   const tableCollection = homeBlocks.find(
     block => block.value.type === "collection_view"
@@ -44,18 +53,10 @@ export const getData = async (): Promise<Output> => {
 
   const posts = convertCollection(collectionData);
 
-  const availableType = ["image", "text"];
-
-  const sections = homeBlocks
-    .filter(block => {
-      return availableType.indexOf(block.value.type) > -1;
-    })
-    .map(convertBlock)
-    .filter(Boolean);
-
   return {
-    posts,
-    sections
+    cardImage,
+    cardText,
+    posts
   };
 };
 
