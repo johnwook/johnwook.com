d2a067c39b7aebff64a5291dbc8e1db4837363fc
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-10-16 16:22:10 +0900

    Tag 를 위한 layout 구성
    - 임시 data 로 넣어둠
    - Link 및 layout 설정
    - Tag 페이지 작성해야 함

--- pages/posts/[pid].tsx
+++ pages/posts/[pid].tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Head from "next/head";
+import Link from "next/link";
 import { NextPage } from "next";
 
 import fetch from "cross-fetch";
 
 import Box from "@material-ui/core/Box";
+import Chip from "@material-ui/core/Chip";
 import NoSsr from "@material-ui/core/NoSsr";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 import format from "date-fns/format";
 
@@ -18,13 +21,22 @@ import { getBaseUrl } from "../../urlHelper";
 
 type Props = PostData;
 
+const useStyles = makeStyles(theme => ({
+  chip: {
+    margin: theme.spacing(0.3)
+  }
+}));
+
 const renderDate = (timestamp: number, title: string) => (
   <NoSsr>
-    <Box>
-      <Typography variant="caption" color="textSecondary">
-        {title + ": " + format(new Date(timestamp), "PPp")}
-      </Typography>
-    </Box>
+    <Typography
+      component="p"
+      variant="caption"
+      color="textSecondary"
+      align="right"
+    >
+      {title + ": " + format(new Date(timestamp), "PPp")}
+    </Typography>
   </NoSsr>
 );
 
@@ -34,27 +46,59 @@ const Post: NextPage<Props> = ({
   lastEditedTime,
   title,
   sections
-}) => (
-  <Layout>
-    <Head>
-      <title>{title}::johnwook.com</title>
-      <meta property="og:url" content={"https://johnwook.com/posts/" + id} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={title + "::johnwook.com"} />
-      <meta property="og:description" content={sections[0].value as string} />
-    </Head>
+}) => {
+  const classes = useStyles({});
 
-    <Box my={3}>
-      <Typography variant="h5">{title}</Typography>
-    </Box>
+  return (
+    <Layout>
+      <Head>
+        <title>{title}::johnwook.com</title>
+        <meta property="og:url" content={"https://johnwook.com/posts/" + id} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title + "::johnwook.com"} />
+        <meta property="og:description" content={sections[0].value as string} />
+      </Head>
 
-    <Box>
-      <PostSections sections={sections} />
-      {renderDate(createdTime, "Created")}
-      {renderDate(lastEditedTime, "Last edited")}
-    </Box>
-  </Layout>
-);
+      <Box my={3}>
+        <Typography variant="h5">{title}</Typography>
+      </Box>
+
+      <Box>
+        <PostSections sections={sections} />
+      </Box>
+
+      <Box>
+        <Link href="/">
+          <Chip
+            clickable
+            color="primary"
+            component="a"
+            label="#book review"
+            size="small"
+            variant="outlined"
+            className={classes.chip}
+          />
+        </Link>
+        <Link href="/">
+          <Chip
+            clickable
+            color="primary"
+            component="a"
+            label="#essay"
+            size="small"
+            variant="outlined"
+            className={classes.chip}
+          />
+        </Link>
+      </Box>
+
+      <Box my={2}>
+        {renderDate(createdTime, "Created")}
+        {renderDate(lastEditedTime, "Last edited")}
+      </Box>
+    </Layout>
+  );
+};
 
 Post.getInitialProps = async ({ query, req }) => {
   const { pid } = query;
