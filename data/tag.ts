1ebf6df86eeb01c31cb14ebd3fdec92b882a1b78
Johnwook Choi <johnwook.choi@huiseoul.com>
2019-10-16 17:49:45 +0900

    tag 를 이용한 post 가져오기

---
+++ data/tag.ts
@@ -0,0 +1,46 @@
+import { queryCollection } from "./notion";
+import { convertCollection, ConvertCollectionOutput } from "./convert";
+
+interface Input {
+  tid: string;
+}
+
+interface Output {
+  tid: string;
+  posts: ConvertCollectionOutput;
+}
+
+const collectionId = "7226e9a0-9a13-454b-866c-03ba71fcb423";
+const collectionViewId = "e34c69b8-2bba-42f0-bcea-e581cc6f18ff";
+
+export const getData = async ({ tid }: Input): Promise<Output> => {
+  const collectionData = await queryCollection({
+    collectionId,
+    collectionViewId,
+    query: {
+      filter: [
+        {
+          property: "1Ote",
+          comparator: "enum_contains",
+          value: tid
+        }
+      ],
+      sort: [
+        {
+          direction: "descending",
+          property: "ns3x",
+          type: "created_time"
+        }
+      ]
+    }
+  });
+
+  const posts = convertCollection(collectionData);
+
+  return {
+    tid,
+    posts
+  };
+};
+
+export type TagData = Output;
